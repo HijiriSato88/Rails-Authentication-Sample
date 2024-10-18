c2a7fcf87a47a90279e0a1cadda50d374bdbc85b
HijiriSato88 <satohiji0808@outlook.com>
2024-10-18 12:15:07 +0900

    ユーザ新規登録追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  
-    def index
-        users = User.where(logged_in: true)
-        render json:{ users:users }, status: :ok
-    end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      render json: { message: 'ユーザーが追加され、ログインに成功しました', user: @user }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
   
+  def index
+    users = User.where(logged_in: true)
+    render json: { users: users }, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users, only: [:index] 
+  resources :users, only: [:index,:create,:new]
 end
