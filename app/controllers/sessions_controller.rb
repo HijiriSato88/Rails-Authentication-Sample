6af47ef9787da06b579714bcec80bbc5264ceff5
HijiriSato88 <satohiji0808@outlook.com>
2024-11-11 11:18:00 +0900

    使用していないAPIの削除

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,6 @@ class SessionsController < ApplicationController
             render json: { message: 'Invalid email or password' }, status: :unauthorized
         end
     end
-
-    def show
-        if current_user
-            render json: { user: current_user }, status: :ok
-        else
-            render json: { message: 'Not logged in' }, status: :unauthorized
-        end
-    end
     
     def destroy
         if logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get    '/login/current',   to: 'sessions#show'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
+  
   resources :users, only: [:index,:create,:new]
 end
